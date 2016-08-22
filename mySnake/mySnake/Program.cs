7221675ffc5e5380269bc4748ba0bb33854470b6
cessel <cessel@yandex.ru>
2016-08-22 14:48:26 +0300

    Создан конструктор класса, рассмотрен принцип инкапсуляции

--- mySnake/mySnake/Program.cs
+++ mySnake/mySnake/Program.cs
@@ -11,18 +11,20 @@ namespace mySnake
         static void Main(string[] args)
         {
 
-            Point p1 = new Point();
-
+            Point p1 = new Point(1,3,'*');
+            /*
             p1.x = 1;
             p1.y = 3;
             p1.sim = '*';
+            */
             p1.Draw();
 
-            Point p2 = new Point();
-
+            Point p2 = new Point(4,5,'#');
+            /*
             p2.x = 4;
             p2.y = 5;
             p2.sim = '#';
+            */
             p2.Draw();
 
            
